668e433da3d1a3729cefb73a313737d90776a51d
anguianoo <njanguiano@gmail.com>
2022-04-20 19:01:07 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+// // Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+// element.textContent = "You've changed what's on the screen!";
+
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
